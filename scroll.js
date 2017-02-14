5438578a18b2fbdfa183729263aefaa15d2424fb
Chuck Dries <chuck@chuckdries.com>
2017-02-14 04:09:15 -0700

    Gross IE hack for getting the scroll position of the window

--- scroll.js
+++ scroll.js
@@ -10,7 +10,7 @@ var startTime = null;
 function smoothScroll(targetId){
     target = document.getElementById(targetId);//grab the element
     targetHeight = target.offsetTop; //get its height
-    currentHeight = window.scrollY; //grab the current scroll position
+    currentHeight = getScrollY(); //grab the current scroll position
     startTime = null; //we have to reset the start time every time the animation runs
     if(currentHeight != targetHeight){
         window.requestAnimationFrame(animateScroll);//schedule the animation
@@ -19,11 +19,11 @@ function smoothScroll(targetId){
 function animateScroll(timeStamp){
     if (startTime == null) startTime = timeStamp; //record the time the animation started if we're just starting
     var deltaStep = (timeStamp - startTime) * speed; //timeStamp - startTime gives us miliseconds since the animation started, we scale this down because its usually in the hundreds.
-    currentHeight = window.scrollY; //record the current scroll position of the window
+    currentHeight = getScrollY(); //record the current scroll position of the window
     var offsetScroll = targetHeight - currentHeight; //calculate how far we still need to go
     var deltaScroll = deltaStep  * offsetScroll; //decide how far to scroll this tick
     window.scroll(0, currentHeight + deltaScroll); //scroll!
-    currentHeight = window.scrollY;//re-record the current height after scrolling for comparison
+    currentHeight = getScrollY();//re-record the current height after scrolling for comparison
     if(currentHeight != targetHeight){ //schedule another tick if we need to
         window.requestAnimationFrame(animateScroll);
     }
