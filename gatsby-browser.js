0e1f3a07255c9dad2138b3c930bddaede320247c
Chuck Dries <chuck@chuckdries.com>
2021-06-28 12:32:39 -0700

    fix build

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,7 +5,7 @@ const env = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'developmen
 if (env === 'production') {
   posthog.init('HR8Gte105aCHNx2BqhL1XkbvH9kzKGptxjkbhuTj6Ek', { api_host: 'https://posthog.chuckdries.com' });
 }
-exports.onRouteUpdate = function () {
+export const onRouteUpdate = function () {
   if (
     process.env.NODE_ENV === 'production' &&
     typeof window.plausible === 'object'
