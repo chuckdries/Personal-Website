7e5d43115ebbb9f2714f48492bd2350c812b92c5
Chuck Dries <chuck@chuckdries.com>
2021-07-17 14:46:06 -0700

    add bg images

--- src/pages/index.js
+++ src/pages/index.js
@@ -314,6 +314,7 @@ export const query = graphql`
             "DSC08521.jpg"
             "DSC07490.jpg"
             "DSC02538.jpg"
+            "DSC06451-3-Enhanced.jpg"
           ]
         }
       }
