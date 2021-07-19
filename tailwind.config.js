b618a6e526586e7667a2eb4cf93d4793609ca91b
Chuck Dries <chuck@chuckdries.com>
2021-07-18 20:13:45 -0700

    add comic neue

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
     fontFamily: {
       ...defaultTheme.fontFamily,
       // serif: ['Didot', 'Didot LT', 'STD', 'Hoefler Text' , 'Garamond', 'Times New Roman', 'serif']
-      serif: ["Playfair Display", "serif"],
+      serif: ["Comic Neue", "serif"],
     },
     extend: {
       dropShadow: {
