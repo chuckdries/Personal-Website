1251ec2a47824cce52f702b613a4d63d8507035e
Chuck Dries <chuck@chuckdries.com>
2022-02-10 11:39:11 -0800

    swap out redwood forest photo and adjust metadataitem spacing

--- src/components/GalleryImage/MetadataItem.js
+++ src/components/GalleryImage/MetadataItem.js
@@ -3,9 +3,9 @@ import React from "react";
 const MetadataItem = ({ icon, data, title }) =>
   data ? (
     <div className="flex justify-end items-end mr-2">
-      <div className="flex flex-col items-end">
-        <span className="font-mono text-sm m-0 mt-2 ">{title}</span>
-        <span className="text-lg whitespace-nowrap">{data}</span>
+      <div className="flex flex-col items-end my-2">
+        <span className="font-mono text-sm opacity-60 m-0 ">{title}</span>
+        <span className="text-lg leading-4 whitespace-nowrap">{data}</span>
       </div>
       <span className="icon-offset ml-2 mt-1 text-2xl">
         <ion-icon name={icon} title={title}></ion-icon>
