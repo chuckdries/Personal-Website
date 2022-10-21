d5edee3383b9d15a11679f6ca238f6caee3ef064
Chuck Dries <chuck@chuckdries.com>
2022-10-20 20:00:26 -0700

    upgrade some deps
    upgrade some deps
    
    
    upgrade some deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   mode: "jit",
   // darkMode: 'media', // or 'media' or 'class'
   theme: {
