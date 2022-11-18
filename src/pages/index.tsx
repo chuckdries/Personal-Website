f97033ad9f834ad5c2f23dcedbdd2ee66feb0372
Chuck Dries <chuck@chuckdries.com>
2022-11-17 17:18:24 -0800

    index page styling on small phones

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -92,7 +92,7 @@ const IndexPage = ({
         />
         <GatsbyImage
           alt=""
-          className="flex-auto m-8 mt-0"
+          className="flex-auto m-4 md:m-8 mt-0 md:mt-0"
           image={img!}
           loading="eager"
           objectFit={browserIsLandscape ? "cover" : "contain"}
