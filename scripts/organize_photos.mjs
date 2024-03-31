0b3c9a951360b7d27982129d005fcb80c4ef35dc
Chuck Dries <chuck@chuckdries.com>
2024-03-30 19:36:02 -0700

    write script for folder migration

---
+++ scripts/organize_photos.mjs
@@ -0,0 +1,39 @@
+import "zx/globals";
+// import exifr from "exifr";
+import * as R from "ramda";
+import { parseDate, DateFormatter } from "@internationalized/date";
+
+const MONTH_FORMATTER = new DateFormatter("en", { month: "long" });
+const YEAR_FORMATTER = new DateFormatter("en", { year: "numeric" });
+
+const files = (
+  await $`exiftool -T -FileName -DateTimeOriginal data/gallery`
+).stdout
+  .split("\n")
+  .map((line) => {
+    let [filename, datetime] = line.split("\t");
+    if (!datetime || !datetime.trim().length) {
+      console.log("⚠️ No datetime for file", filename)
+      return { filename, dir: null };
+    }
+    datetime = datetime.split(" ")[0].replace(/:/g, "-");
+    const date = parseDate(datetime);
+    const dir = `${YEAR_FORMATTER.format(date.toDate())}/${MONTH_FORMATTER.format(date.toDate())}`;
+    return { filename, dir};
+  });
+
+const dirs = R.groupBy(R.prop("dir"), files);
+
+console.log(dirs);
+
+for (const dir in dirs) {
+  if (dir === null) {
+    continue;
+  }
+  console.log(`Creating directory: ${dir}`);
+  await $`mkdir -p data/photos/${dir}`;
+  for (const file of dirs[dir]) {
+    console.log(`Copying file ${file.filename} to ${dir}`);
+    await $`cp data/gallery/${file.filename} data/photos/${dir}`;
+  }
+}
