cfd987c852fbeeefabd46c6b5de99e304d65dd5c
Chuck Dries <chuck@chuckdries.com>
2024-12-30 02:14:29 -0700

    scroll prompt style

--- src/components/Posts/PostImageGroup.tsx
+++ src/components/Posts/PostImageGroup.tsx
@@ -27,7 +27,7 @@ export function PostImageGroup({ children, label }: PostImageGroupProps) {
   }, [containerWidth]);
 
   return (
-    <div style={{ height: childrenHeight ? childrenHeight : '90vh' }}>
+    <div style={{ height: childrenHeight ? childrenHeight : "90vh" }}>
       {/* <div className="h-screen"> */}
       <div
         className="justify-center absolute left-0 right-0 flex overflow-y-hidden"
@@ -37,7 +37,11 @@ export function PostImageGroup({ children, label }: PostImageGroupProps) {
           className="flex flex-nowrap items-center overflow-x-auto w-fit px-3 lg:px-6 gap-6 pb-2"
           ref={observeInner}
         >
-          {showScrollPrompt && <p className="text-nowrap lg:mx-[3vw]">scroll &rarr;</p>}
+          {showScrollPrompt && (
+            <p className="text-nowrap lg:text-lg lg:mx-6 italic opacity-50 bg-[var(--tw-prose-body)] text-white px-3 rounded-full">
+              scroll &rarr;
+            </p>
+          )}
           {/* <p>{innerWidth} &gt; {containerWidth}</p> */}
           {children}
         </div>
